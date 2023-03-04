9bed47490fee77c87ce6a55b5104f38e1eb0a023
haha1haka <haha1haka@icloud.com>
2023-03-05 00:26:09 +0900

    [Docs] file

--- upsplash/UpSplash/Sources/Data/Network/Unsplash/UpsplashAPIKey.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum UnsplashAPIKey {
-    static let appKey = "yyQ4PqsmUlRItIZVR2fLsSpNUO_xrZpli9xxqBqA5uk"
-    static let secretKey = "waEU78UTNwV7rMarK2523uusNLQM4z0oKUzvJdBIdj0"
-}
